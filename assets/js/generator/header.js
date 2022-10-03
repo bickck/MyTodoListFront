b74d74e75a63dec3550c9db9f55c92a8d255cc91
bickck <3d1935@naver.com>
2022-10-03 21:04:36 +0900

    헤더 업데이트

---
+++ assets/js/generator/header.js
@@ -0,0 +1,52 @@
+const headerTagId = document.querySelector("#header");
+// const links = document.querySelectorAll(".link");
+// function header() {
+//     var mainLogoTag = document.createElement("h1");
+//     var logoLink = document.createElement("a");
+//     var linksNav = document.createElement("nav");
+//     var linksUl = document.createElement("ul");
+//     var linksLi = document.createElement("li");
+//     var aLink = document.createElement("a");
+//     var mainNav = document.createElement("nav");
+//     var mainUl = document.createElement("ul");
+//     var childLiSearch = document.createElement("li");
+//     var searchForm = document.createElement("form");
+//     var childLiMenu = document.createElement("li");
+    
+//      logoLink.setAttribute("href","index.html");
+//      mainLogoTag.appendChild(logoLink);
+
+//      linksNav.setAttribute("class","links");
+
+//      //-- li --
+
+
+//      //-- li --
+
+
+//      linksNav.appendChild(linksUl);
+
+//      mainNav.setAttribute("class","main");
+
+
+
+//      mainUl.appendChild(childLiSearch);
+//      mainUl.appendChild(childLiMenu);
+
+
+// }
+
+//const links = headerTagId.children[1];
+
+
+function header() {
+    links[0].innerText = "Quote";
+    console.dir(links[0]);
+    console.log(links[1]);
+    console.log(links[2]);
+    console.log(links[3]);
+    console.log(links[4]);
+ 
+}
+
+// header();
