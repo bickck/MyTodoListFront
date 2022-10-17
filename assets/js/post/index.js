edfeb91c425107b70ea8878a3b57a21462e5aad7
bickck <3d1935@naver.com>
2022-10-17 22:18:22 +0900

    테스트

--- assets/js/post/index.js
+++ assets/js/post/index.js
@@ -7,17 +7,19 @@ import {
 
 import {
     Todo
-} from "../server/Todo.js"
+} from "../server/Todo.js";
 
 import {
     Users
-} from "../server/Users.js"
+} from "../server/Users.js";
 
 
 const mainTodos = new Todo();
 const auth = new Auth();
 const post = new Post();
 const user = new Users();
+
+
 const mainPost = document.querySelector("#main");
 
 window.onload = function init(event) {
