843e4a7b27ff287fa3628930f4c43fb3a9f0c3f3
bickck <3d1935@naver.com>
2022-12-29 11:47:41 +0900

    링크 수정

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,6 +1,7 @@
 // product IP
 //const ipAddress = "52.78.161.7";
 //const backEndServerAddress = `https://${ipAddress}:8080`;
+const backEndServerAddress = `https://www.mybackendpractice.link`
 const frontEndServerAddress = "https://bickck.github.io/MyTodoListFront"
 const mainPageAddress = "https://bickck.github.io/MyTodoListFront";
 
